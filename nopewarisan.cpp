9583ba53658ed072a037226137b2958481534090
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-05-19 18:40:35 +0700

    first commit

---
+++ nopewarisan.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya function dari base class";
+    }
+};
+class baseClass1 final
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya function dari base class";
+    }
+};
+class derivedClass : public baseClass1
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya function dari derived class";
+    }
+};
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+    a.perkenalan();
+    return 0;
+}
